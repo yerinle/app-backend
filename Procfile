42730626cb42822819e720e0a659211e9fc6258a
Bayo Erinle <berinle@gmail.com>
2014-03-22 11:09:55 -0400

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/app-backend-0.0.1.jar
---
+++ app.groovy
@@ -0,0 +1,3 @@
+@GrabResolver(name='app-ui', root='http://dl.bintray.com/berinle/maven')
+@Grab('app-ui:app-ui:0.0.1')
+@Controller class app{}
