4ff10c1e835f3e3c6b8171d5c0b322bb6e1a8fb1
Yinka Erinle <yerinle@gmail.com>
2014-04-26 20:54:36 +0100

    Update to use local repository to resolve app-ui jar

--- app.groovy
+++ app.groovy
@@ -1,3 +1,5 @@
-@GrabResolver(name='app-ui', root='http://dl.bintray.com/berinle/maven')
-@Grab('app-ui:app-ui:0.0.1')
+//add below to ~/.groovy/grapeConfig.xml 
+//<ibiblio name="local" root="file:${user.home}/.foa/repository/" m2compatible="true"/>
+
+@Grab('jrock:app-ui:0.0.1')
 @Controller class app{}
